65e21a2640d335c764a4923d921a8d02dbc167ed
W0412692 <55142471+W0412692@users.noreply.github.com>
2024-01-23 10:36:16 -0800

    Add files via upload
    merge

--- src-backend/server.py
+++ src-backend/server.py
@@ -9,4 +9,7 @@ allowed_methods = ["GET"]
 @app.route("/api", methods=allowed_methods)
 def hello_world():
     return {"a": "B"}
-    
+
+@app.route("/asdasdasd", methods=allowed_methods)
+def xswdvfrgbyhjukil():
+    return "woah, what an efficient way of setting up a backend"
